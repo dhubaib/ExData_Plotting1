1db3fbf857664688a163964b93fe15bcee7e8b3d
Cal <cal@tripleanalytics.com>
2015-11-08 23:25:24 -0500

    Plot2

--- plot1.R
+++ plot1.R
@@ -1,7 +1,9 @@
-## Script to create 
+## Script to create plot1
 source("plotter.R") # Read in data as "Dat"
 
 # export plot
 png(file = "plot1.png",width = 480, height = 480)
-hist(dat$Global_active_power,col="red")
+hist(dat$Global_active_power,col="red",
+     main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)")
 dev.off()
---
+++ plot2.R
@@ -0,0 +1,11 @@
+## Script to create plot2
+source("plotter.R") # Read in data as "Dat"
+
+# export plot
+png(file = "plot2.png",width = 480, height = 480)
+plot(as.POSIXct(dat$datetime), dat$Global_active_power,
+     type = "l",
+     main = "",
+     xlab = "",
+     ylab = "Global Active Power (kilowatts)")
+dev.off()
